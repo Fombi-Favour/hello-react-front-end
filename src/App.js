2b9213605c3967fb9f2a82c47229d4f7c18e190e
Fombi Favour <fombifavour@gmail.com>
2024-02-26 12:20:37 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Greetings from './components/Greetings';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Greetings />} />
+    </Routes>
+  );
+}
+
+export default App;
