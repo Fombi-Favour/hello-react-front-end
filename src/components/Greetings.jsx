2951858f3789b21ccff8ba79d923b20caad92c8c
Fombi Favour <fombifavour@gmail.com>
2024-02-26 14:23:22 +0100

    Update Greetings.jsx

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greetings/greetingSlice';
 
 const Greetings = () => {
+  const dispatch = useDispatch();
+  const greet = useSelector((state) => state.greeting.message);
+
+  useEffect(() => {
+    if (!greet) {
+      dispatch(fetchGreeting())
+        .then(() => {
+          console.log('Success!!!');
+        }).catch(() => { console.log('Error!!!'); });
+    }
+  }, [dispatch]);
+
   return (
-    <div>Greetings</div>
-  )
-}
+    <div>
+      {greet}
+    </div>
+  );
+};
 
 export default Greetings;
